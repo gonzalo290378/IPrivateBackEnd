ce671f4c472b5012e093afd2bd02d0d6df8edb29
unknown <designanimation.inc@gmail.com>
2025-04-08 19:57:58 -0300

    ParentPom configuration

--- ms-gateway/src/main/java/org/gdegenaro/springlcloud/msvcgateway/MsvcGatewayApplication.java
+++ ms-gateway/src/main/java/org/gdegenaro/springlcloud/msvcgateway/MsvcGatewayApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 public class MsvcGatewayApplication {
 
-	//Orden ejecucion ms: Eureka, AuthServer, MsUsers, MsAccounts, MsPayments, MsQueue, ApiGateway
+	//Orden ejecucion ms: Eureka, ConfigServer, MsUsers, MsAccounts, MsPayments, MsQueue, ApiGateway
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsvcGatewayApplication.class, args);
