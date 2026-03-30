b6a141b3a1a50cb1a82de6e978127626d71550ba
gonzalo290378 <gonzalo29031978@hotmail.com>
2026-03-30 12:59:30 +0200

    feature UX public and edit profile 1.2.0

---
+++ ms-follow/src/main/java/com/iprivado/ms_follow/IprivateApplication.java
@@ -0,0 +1,13 @@
+package com.iprivado.ms_follow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IprivateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IprivateApplication.class, args);
+	}
+
+}
